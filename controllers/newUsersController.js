eee59618e50dafd376647577b9e4fe91d222247e
marefpceo <marefpceo@gmail.com>
2024-08-02 16:32:02 -0700

    Completed basic username add form

--- controllers/newUsersController.js
+++ controllers/newUsersController.js
@@ -1,11 +1,29 @@
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
 
 // Display form to input username
-exports.get_new = asyncHandler(async (req, res) => {
-  res.render('new', { title: 'New Username Form' });
+exports.get_new = asyncHandler(async (req, res, next) => {
+  res.render('usernameForm', { title: 'New Username Form' });
 });
 
 // Handle saving new username to DB
-exports.post_new = asyncHandler(async (req, res) => {
-  console.log('username to be saved: ', req.body.username);
-})
+exports.post_new = [
+  body('username')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .isAlphanumeric()
+    .withMessage('First name has non-alphanumeric characters.'),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty) {
+      res.render('usernameForm', {
+        errors: errors.array()
+      });
+    } else {
+      res.send(`Handle saving new username to DB. Username: ${req.body.username}`);
+    }
+  }),
+];
