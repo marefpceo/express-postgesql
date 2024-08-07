450921e94ebcebb533eee756bf032a34b5e0cf1a
marefpceo <marefpceo@gmail.com>
2024-08-07 14:33:10 -0700

    Started working search query

--- db/queries.js
+++ db/queries.js
@@ -9,6 +9,10 @@ async function insertUsername(username) {
   await pool.query('INSERT INTO usernames (username) VALUES ($1)', [username]);
 }
 
+async function searchDatabase(searchInput) {
+  const { rows } = await pool.query('SELECT * FROM usernames WHERE (searchInput) VALUES ($1)', [searchInput]);
+}
+
 module.exports = {
   getAllUsernames,
   insertUsername
